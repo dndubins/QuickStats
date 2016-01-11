5a56f845b5f71357aac36d203a76637318cbd555
dndubins <d.dubins@utoronto.ca>
2016-01-10 22:18:29 -0500

    Create QuickStats.h

---
+++ QuickStats.h
@@ -0,0 +1,27 @@
+/*  QuickStats.h - Library for quick descriptive statistics of an array samples[] of size m,
+ *  assuming a normal distribution.
+ *  Created by David Dubins, January 10th, 2016.
+ *  Released into the public domain.
+ */
+
+#ifndef QuickStats_h
+#define QuickStats_h
+
+#include <Arduino.h>
+
+class QuickStats {
+  public:
+    QuickStats();
+    ~QuickStats();
+    float average(float samples[],int m);
+    float minimum(float samples[],int m);
+    float maximum(float samples[],int m);	
+    float stdev(float samples[],int m);
+    float stderror(float samples[],int m);
+    float CV(float samples[],int m);
+    void bubbleSort(float A[],int len);
+    float median(float samples[],int m);
+    float mode(float samples[],int m);
+};
+
+#endif
