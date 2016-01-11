c98a5f585256d6593daecda05f0cd1ee4c5f398b
dndubins <d.dubins@utoronto.ca>
2016-01-10 22:23:27 -0500

    Create statistics.ino

---
+++ examples/statistics/statistics.ino
@@ -0,0 +1,34 @@
+// Example program for use with QuickStats.h 
+
+#include "QuickStats.h"
+ 
+int numreadings = 9;
+float readings[]={1.0,2.2,4.8,3.3,6.1,2.2,3.8,7.0,2.2};
+ 
+QuickStats stats; //initialize an instance of this class
+
+void setup()
+{
+  Serial.begin(9600); 
+  Serial.println("Descriptive Statistics");
+  Serial.print("Average: ");
+  Serial.println(stats.average(readings,numreadings));
+  Serial.print("Minimum: ");
+  Serial.println(stats.minimum(readings,numreadings));
+  Serial.print("Maximum: ");
+  Serial.println(stats.maximum(readings,numreadings));
+  Serial.print("Standard Deviation: ");
+  Serial.println(stats.stdev(readings,numreadings));
+  Serial.print("Standard Error: ");
+  Serial.println(stats.stderror(readings,numreadings));
+  Serial.print("Coefficient of Variation (%): ");
+  Serial.println(stats.CV(readings,numreadings));
+  Serial.print("Median: ");
+  Serial.println(stats.median(readings,numreadings));
+  Serial.print("Mode: ");
+  Serial.println(stats.mode(readings,numreadings));
+}
+ 
+void loop()
+{
+}
