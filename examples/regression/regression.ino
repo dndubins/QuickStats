072b2eb9ae8e9fe72973e84a905d8400e1c7e6bb
dndubins <d.dubins@utoronto.ca>
2018-06-09 02:18:08 -0400

    Add files via upload
    Example linear regression.

---
+++ examples/regression/regression.ino
@@ -0,0 +1,24 @@
+// Example program for use with QuickStats.h 
+// Simple linear regression (y=ax+b)
+
+#include "QuickStats.h"
+ 
+int numreadings = 5;
+float readings[]={2.15, 3.25, 2.55, 4.64, 7.43};
+float times[]={0.00, 0.10, 0.20, 0.41, 0.62};
+ 
+QuickStats stats; //initialize an instance of this class
+
+void setup()
+{
+  Serial.begin(9600); 
+  Serial.println("Linear Regression");
+  Serial.print("Slope: ");
+  Serial.println(stats.slope(times,readings,numreadings));
+  Serial.print("Intercept: ");
+  Serial.println(stats.intercept(times,readings,numreadings));
+}
+ 
+void loop()
+{
+}
