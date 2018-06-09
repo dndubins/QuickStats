66392e998773f400914208389c9ee70cd62b9786
dndubins <d.dubins@utoronto.ca>
2018-06-09 03:08:06 -0400

    Add files via upload
    Sliding average example.

---
+++ examples/slidingavg/slidingavg.ino
@@ -0,0 +1,39 @@
+// Example program for use with QuickStats.h 
+// Report sliding average with slope
+
+#include "QuickStats.h"
+
+#define NUMREADINGS 10
+
+byte vPin=A0;
+float readings[NUMREADINGS];
+float times[NUMREADINGS];
+int idx=NUMREADINGS;
+int count=0;
+
+QuickStats stats; //initialize an instance of this class
+
+void setup()
+{
+  Serial.begin(9600); 
+  Serial.println("Reading, Sliding Average, Slope");
+}
+ 
+void loop()
+{
+  count++;
+  idx++;
+  if(idx>=NUMREADINGS){
+    idx=0;
+  }
+  times[idx]=millis();
+  readings[idx]=analogRead(vPin);
+  if(count>NUMREADINGS){
+    Serial.print(readings[idx]);
+    Serial.print(", ");
+    Serial.print(stats.average(readings,NUMREADINGS));
+    Serial.print(", ");
+    Serial.println(stats.slope(times,readings,NUMREADINGS));
+  }
+  delay(50);
+}
