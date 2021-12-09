c83ab4e08abf6a258a5200c6b549b052fb4ac7ca
David Dubins <d.dubins@utoronto.ca>
2021-12-08 20:01:22 -0500

    Update Quickstats.h

--- standalone/Quickstats.h
+++ standalone/Quickstats.h
@@ -1,50 +1,12 @@
 /*************************************************************
  * Quick Statistics for Float Arrays, by David Dubins
  * 02-Dec-2020
- * http://pb860.pbworks.com
+ * Last Updated: 08-Dec-21
+ * https://github.com/dndubins/QuickStats/edit/master/standalone/Quickstats.h
  * For a more functional version of this library, visit:
  * https://github.com/dndubins/QuickStats
  *************************************************************/
 #include <math.h>
-//#define DEBUG
-
-float average(float samples[],int m)
-{
-  float avg1=0.0;
-  for(int i=0;i<m;i++){ 
-    avg1+=(samples[i]-avg1)/(i+1); // iterative mean calculated here
-  }
-  return avg1;
-}
-
-float g_average(float samples[],int m)
-{
-  float avg1=0.0;
-  for(int i=0;i<m;i++){
-    avg1+=(log(samples[i])-avg1)/(i+1); // iterative geometric mean calculated here
-  }
-  return exp(avg1);
-}
-
-float stdev(float samples[],int m)
-{
-  float avg=0.0;
-  float total2=0.0;
-  avg=average(samples,m);
-  for(int i=0;i<m;i++){
-    total2 = total2 + pow(samples[i] - avg,2);
-  }
-  return sqrt(total2/((float)(m-1)));
-}
-
-float CV(float samples[],int m)  //Coefficient of variation (%RSD, or relative stdev)
-{
-  float avg=0.0;
-  float sd=0.0;
-  avg=average(samples,m);
-  sd=stdev(samples,m);
-  return 100.0*sd/avg;
-}
 
 void bubbleSort(float A[],int len)
 {
@@ -65,65 +27,132 @@ void bubbleSort(float A[],int len)
   } while(n>1);
 }
 
+float average(float samples[],int m)
+{
+  float avg1=0.0;
+  for(int i=0;i<m;i++){ 
+    avg1+=(samples[i]-avg1)/(i+1); // iterative mean calculated here
+  }
+  return avg1;
+}
+
+float g_average(float samples[],int m)
+{
+  float avg1=0.0;
+  for(int i=0;i<m;i++){
+    avg1+=(log(samples[i])-avg1)/(i+1); // iterative geometric mean calculated here
+  }
+  return exp(avg1);
+}
+
+float minimum(float samples[],int m)
+{
+  float sorted[m];   //Define and initialize sorted array
+  for(int i=0;i<m;i++){
+    sorted[i]=samples[i];
+  }
+  bubbleSort(sorted,m);  // Sort the values
+  return(sorted[0]); // first element is the minimum
+}
+
+float maximum(float samples[],int m)
+{
+  float sorted[m];   //Define and initialize sorted array
+  for(int i=0;i<m;i++){
+    sorted[i]=samples[i];
+  }
+  bubbleSort(sorted,m);  // Sort the values
+  return(sorted[m-1]);   // last element is the maximum
+}
+
+float stdev(float samples[],int m)
+{
+  float avg=0.0;
+  float total2=0.0;
+  avg=average(samples,m);
+  for(int i=0;i<m;i++){
+    total2 = total2 + pow(samples[i] - avg,2);
+  }
+  return sqrt(total2/((float)m-1.0));
+}
+
+float stderror(float samples[],int m)
+{
+  float temp1=0.0;
+  temp1=stdev(samples,m);
+  return (temp1/sqrt((float)m));
+}
+
+float CV(float samples[],int m)  //Coefficient of variation (%RSD, or relative stdev)
+{
+  float avg=0.0;
+  float sd=0.0;
+  avg=average(samples,m);
+  sd=stdev(samples,m);
+  return 100.0*sd/avg;
+}
+
+float fabs(float sample) // calculate absolute value
+{ 
+  if(sample<0.f){
+    return -sample;
+  }else{
+    return sample;
+  }
+}
+
 float median(float samples[],int m) //calculate the median
 {
   //First bubble sort the values: https://en.wikipedia.org/wiki/Bubble_sort
   float sorted[m];   //Define and initialize sorted array.
   float temp=0.0;      //Temporary float for swapping elements
-  #ifdef DEBUG
-    Serial.println("Before:");
-    for(int j=0;j<m;j++){
-      Serial.println(samples[j]);
-    }
-  #endif
+  /*Serial.println("Before:");
+  for(int j=0;j<m;j++){
+    Serial.println(samples[j]);
+  }*/
   for(int i=0;i<m;i++){
     sorted[i]=samples[i];
   }
   bubbleSort(sorted,m);  // Sort the values
-  #ifdef DEBUG
-    Serial.println("After:");
-    for(int i=0;i<m;i++){
-      Serial.println(sorted[i]);
-    }
-  #endif
-  if (bitRead(m,0)) {  //If the last bit of a number is 1, it's odd. This is equivalent to "TRUE". Also use if m%2!=0.
+  /*Serial.println("After:");
+  for(int i=0;i<m;i++){
+    Serial.println(sorted[i]);
+  }*/
+  if (bitRead(m,0)==1) {  //If the last bit of a number is 1, it's odd. This is equivalent to "TRUE". Also use if m%2!=0.
     return sorted[m/2]; //If the number of data points is odd, return middle number.
   } else {    
-    return (sorted[(m/2)-1]+sorted[m/2])/2.0; //If the number of data points is even, return avg of the middle two numbers.
+    return (sorted[(m/2)-1]+sorted[m/2])/2; //If the number of data points is even, return avg of the middle two numbers.
   }
 }
 
-float mode(float samples[],int m) //calculate the mode. 
+float mode(float samples[],int m,float epsilon) //calculate the mode.
+//epsilon is the tolerance for two measurements to be equivalent.
 {
   //First bubble sort the values: https://en.wikipedia.org/wiki/Bubble_sort
   float sorted[m];   //Temporary array to sort values.
   float temp=0;      //Temporary float for swapping elements
   float unique[m];   //Temporary array to store unique values
-  int uniquect[m];   //Temporary array to store unique counts
-  #ifdef DEBUG
-    Serial.println("Before:");
-    for(int i=0;i<m;i++){
-      Serial.println(samples[i]);
-    }
-  #endif
+  int uniquect[m]; //Temporary array to store unique counts
+  /*Serial.println("Before:");
+  for(int i=0;i<m;i++){
+    Serial.println(samples[i]);
+  }*/
   for(int i=0;i<m;i++){
     sorted[i]=samples[i];
   }
   bubbleSort(sorted,m);  // Sort the values
-  #ifdef DEBUG
-    Serial.println("Sorted:");
-    for(int i=0;i<m;i++){
-      Serial.println(sorted[i]);
-    }
-  #endif
+  /*Serial.println("Sorted:");
+  for(int i=0;i<m;i++){
+    Serial.println(sorted[i]);
+  }*/
   // Now count the number of times each unique number appears in the sorted array.
   unique[0]=sorted[0];
   uniquect[0]=1;
   int p=0; // counter for # unique numbers
   int maxp=0;
   int maxidx=0;
-  for(int i=1;i<m;i++){
-    if(abs(sorted[i]-sorted[i-1])<0.0001){  // tolerance for two readings being the same. Set as needed.
+  for(int i=1;i<m;i++){ 
+    if(fabs(sorted[i]-sorted[p])<epsilon){
       uniquect[p]++;  //if same number again, add to count
       if(uniquect[p]>maxp){
         maxp=uniquect[p];
@@ -135,14 +164,65 @@ float mode(float samples[],int m) //calculate the mode.
       uniquect[p]=1;
     }
   }
-  #ifdef DEBUG
-    for(int i=0;i<p+1;i++){
-      Serial.println("Num: " + (String)unique[i] +"   Count: " + (String)uniquect[i]);
-    }
-  #endif  
+  /*for(int i=0;i<p+1;i++){ //uncomment this section for debugging
+    Serial.print("Num: "); 
+    Serial.print(unique[i],4);
+    Serial.print(" Count: "); 
+    Serial.println(uniquect[i]);
+  }*/ 
   if (maxp>1) {    
     return unique[maxidx]; //If there is more than one mode, return the lowest one.
   } else {
     return 0.0; //If there is no mode, return a zero.
+  }
+}
+
+float slope(float x[],float samples[],int m)  //calculate the slope (dsamples/dx)
+{
+  float xavg=average(x,m);
+  float yavg=average(samples,m);
+  float numerator = 0.0;
+  float denominator = 0.0;
+  for(int i=0;i<m;i++){
+    if(x[i]-xavg!=0.0){ // protect against dividing by zero
+      numerator = numerator + (x[i]-xavg)*(samples[i]-yavg);
+      denominator = denominator + ((x[i]-xavg)*(x[i]-xavg));
+    }
+  }
+  return numerator/denominator;  
+}
+
+float intercept(float x[],float samples[],int m)  //calculate the intercept (dsamples/dx)
+{
+  float xavg=average(x,m);
+  float yavg=average(samples,m);
+  float beta=slope(x,samples,m);
+  return yavg-(beta*xavg);
+}
+
+void filternan(float samples[],int &m)  //removes nan values and returns size of filtered matrix (destructive)
+{
+  int duds=0; //keep track of #nans
+  int nums=0; //keep track of numbers
+  float filtered[m];
+  for(int i=0;i<m;i++){
+    if(isnan(samples[i])||isinf(samples[i])){
+      duds++; // found a nan
+    }else{
+      filtered[nums]=samples[i];
+      nums++;  // found a number
+    }
+  }
+  for(int i=0;i<nums;i++){
+    samples[i]=filtered[i]; //overwrite sample matrix with filtered matrix 
+  }
+  m=nums; //overwrite matrix size
+}
+
+void f_round(float samples[], int m, int p)  //round float variable to a given # decimals, p
+{
+  float precision=pow(10.0,p);
+  for(int i=0;i<m;i++){
+    samples[i]=round(samples[i]*precision)/precision;
   }
 }
